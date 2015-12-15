e07ff1c3f5f0ba11d0d4a08cab85fea730e5ccc0
Michal Rostecki <mrostecki@mirantis.com>
2015-12-15 15:06:21 +0100

    Add script for cleanup
    Command kolla-mesos-cleanup removes all kolla-related nodes
    in ZooKeeper as well as all the entities in Marathon and Chronos.
    
    Change-Id: If48c37760f4ee0d639a0555346bb9f7bcfaf3758
    Closes-Bug: 1526327

---
+++ kolla_mesos/cleanup.py
@@ -0,0 +1,25 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from kolla_mesos import chronos
+from kolla_mesos.common import zk_utils
+from kolla_mesos import marathon
+
+
+def cleanup():
+    marathon_client = marathon.Client()
+    chronos_client = chronos.Client()
+
+    with zk_utils.connection() as zk:
+        zk_utils.clean(zk)
+    marathon_client.remove_all_apps()
+    chronos_client.remove_all_jobs()
