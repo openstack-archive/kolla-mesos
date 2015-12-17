5f012252aa5a7a71e95e9d9b1072e495dad2c422
Michal Rostecki <mrostecki@mirantis.com>
2015-12-17 18:27:33 +0100

    Fix network settings in Vagrant
    Change-Id: Ie35da4891a0417065be2a493f1455cf9587681c8

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -32,3 +32,10 @@ sudo -u vagrant bash -c "cd ~vagrant/kolla-mesos && tox -e genconfig"
 mkdir -p /etc/kolla-mesos
 cp -r ~vagrant/kolla/etc/kolla/ /etc/kolla
 cp ~vagrant/kolla-mesos/etc/kolla-mesos.conf.sample /etc/kolla-mesos/kolla-mesos.conf
+
+# Change network settings
+# TODO(nihilifer): Change kolla_internal_address when loadbalancing will be implemented.
+HOST_IP=$(ip addr show eth1 | grep -Po 'inet \K[\d.]+')
+sed -i -r "s,^[# ]*kolla_internal_address:.+$,kolla_internal_address: \"$HOST_IP\"," /etc/kolla/globals.yml
+sed -i -r "s,^[# ]*network_interface:.+$,network_interface: \"eth1\"," /etc/kolla/globals.yml
+sed -i -r "s,^[# ]*neutron_external_interface:.+$,neutron_external_interface: \"eth2\"," /etc/kolla/globals.yml
