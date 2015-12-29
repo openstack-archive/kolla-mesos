5baac12df95015b34de752639827d04b1cbfbf06
Michal Rostecki <mrostecki@mirantis.com>
2015-12-29 16:21:38 +0100

    [WIP] Vagrant multinode deployment
    Change-Id: Ib2ddfe80159211363ec5e96ef904388d5870927d
    Depends-On: I2ca57010c45710635cfe80ff23a2a5e2edabee57
    Implements: blueprint vagrant
    Implements: blueprint mesos-docker-images

---
+++ kolla_mesos/cmd/ansible_deploy.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+
+# NOTE(nihilifer): We have to import ansible.utils first, because of the
+# cilcular import issue in Ansible:
+# https://bugzilla.redhat.com/show_bug.cgi?id=1065251
+from ansible import utils as ansible_utils  # flake8: noqa
+from ansible import callbacks
+from ansible import playbook as ansible_playbook
+from kolla.cmd import build as kolla_build
+from oslo_config import cfg
+
+from kolla_mesos.common import file_utils
+
+
+CONF = cfg.CONF
+CONF.import_opt('inventory_path', 'kolla_mesos.config.ansible_cli')
+
+
+def main():
+    CONF(sys.argv[1:], project='kolla-mesos')
+
+    playbook_path = file_utils.find_file('ansible/site.yml')
+    kolla_module_path = os.path.join(kolla_build.find_base_dir(),
+                                     'ansible/library')
+
+    playbook_stats = callbacks.AggregateStats()
+    playbook_callbacks = callbacks.PlaybookCallbacks(
+        verbose=ansible_utils.VERBOSITY)
+    runner_callbacks = callbacks.PlaybookRunnerCallbacks(
+        playbook_stats, verbose=ansible_utils.VERBOSITY)
+
+    playbook = ansible_playbook.PlayBook(playbook=playbook_path,
+                                         host_list=CONF.inventory_path,
+                                         stats=playbook_stats,
+                                         callbacks=playbook_callbacks,
+                                         runner_callbacks=runner_callbacks,
+                                         module_path=kolla_module_path)
+    playbook.run()
+
+
+if __name__ == '__main__':
+    main()
