fd87f0a2629a3a6757c476cfade5b936d905868e
Angus Salkeld <asalkeld@mirantis.com>
2016-01-27 09:11:49 +1000

    Add api_interface and service_name to hostvars
    This is needed by nova and/or heat services
    
    Change-Id: I5ed9d9bebca4b3fb9054e8ab15fad37e20de496a

--- kolla_mesos/tests/container_scripts/test_start.py
+++ kolla_mesos/tests/container_scripts/test_start.py
@@ -482,6 +482,7 @@ class HostvarsAndGroupsTest(base.BaseTestCase):
         exp = {'ansible_eth1': {'ipv4': {'address': '1.2.3.4'}},
                'ansible_eth2': {'ipv4': {'address': '1.2.3.4'}},
                'ansible_hostname': 'test-hostname',
+               'api_interface': 'eth2',
                'role': 'testr',
                'id': '1'}
         self.assertEqual(exp, hostvars['1.2.3.4'])
@@ -500,6 +501,7 @@ class HostvarsAndGroupsTest(base.BaseTestCase):
         remote = {'ansible_eth0': {'ipv4': {'address': '4.4.4.4'}},
                   'ansible_eth1': {'ipv4': {'address': '4.4.4.4'}},
                   'ansible_hostname': 'the-other-host',
+                  'api_interface': 'eth2',
                   'role': 'testr',
                   'id': '55'}
         party.Party(self.client, '/kolla/deploy_id/groups/testg',
@@ -512,6 +514,7 @@ class HostvarsAndGroupsTest(base.BaseTestCase):
         exp_local = {'ansible_eth1': {'ipv4': {'address': '1.2.3.4'}},
                      'ansible_eth2': {'ipv4': {'address': '1.2.3.4'}},
                      'ansible_hostname': 'test-hostname',
+                     'api_interface': 'eth2',
                      'role': 'testr',
                      'id': '1'}
         self.assertEqual(exp_local, hostvars['1.2.3.4'])
