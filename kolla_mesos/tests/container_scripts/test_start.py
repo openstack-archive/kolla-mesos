50cd702942ca877211237a19ee9fe9fc6982767f
Angus Salkeld <asalkeld@mirantis.com>
2016-02-19 13:17:58 +1000

    remove unused get_new_node_id() in start.py
    Change-Id: I649a25505057ee63210e89de6b49638893cc0eb2

--- kolla_mesos/tests/container_scripts/test_start.py
+++ kolla_mesos/tests/container_scripts/test_start.py
@@ -443,8 +443,7 @@ class HostvarsAndGroupsTest(base.BaseTestCase):
         exp = {'ansible_eth1': {'ipv4': {'address': '1.2.3.4'}},
                'ansible_eth2': {'ipv4': {'address': '1.2.3.4'}},
                'ansible_hostname': 'test-hostname',
-               'api_interface': 'eth2',
-               'id': '1'}
+               'api_interface': 'eth2'}
         self.assertEqual(exp, hostvars['1.2.3.4'])
 
     @mock.patch('socket.gethostname')
@@ -460,8 +459,7 @@ class HostvarsAndGroupsTest(base.BaseTestCase):
         remote = {'ansible_eth0': {'ipv4': {'address': '4.4.4.4'}},
                   'ansible_eth1': {'ipv4': {'address': '4.4.4.4'}},
                   'ansible_hostname': 'the-other-host',
-                  'api_interface': 'eth2',
-                  'id': '55'}
+                  'api_interface': 'eth2'}
         party.Party(self.client, '/kolla/deploy_id/groups/testr',
                     json.dumps(remote)).join()
 
@@ -472,8 +470,7 @@ class HostvarsAndGroupsTest(base.BaseTestCase):
         exp_local = {'ansible_eth1': {'ipv4': {'address': '1.2.3.4'}},
                      'ansible_eth2': {'ipv4': {'address': '1.2.3.4'}},
                      'ansible_hostname': 'test-hostname',
-                     'api_interface': 'eth2',
-                     'id': '1'}
+                     'api_interface': 'eth2'}
         self.assertEqual(exp_local, hostvars['1.2.3.4'])
         self.assertEqual(remote, hostvars['4.4.4.4'])
 
