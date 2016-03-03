cbcc0d95604c3483c3e9c9eeb1836fa21e620e6f
Angus Salkeld <asalkeld@mirantis.com>
2016-03-03 11:33:27 +1000

    Make write_file return if the contents changed
    Change-Id: I594b67bf37c03a90c0ca1354f50f61cdbad74039

--- kolla_mesos/container_scripts/start.py
+++ kolla_mesos/container_scripts/start.py
@@ -15,6 +15,7 @@
 import contextlib
 import datetime
 import fcntl
+import filecmp
 import json
 import logging
 import math
@@ -248,6 +249,11 @@ def register_group_and_hostvars(zk):
 
 
 def write_file(conf, data):
+    """Write the data to the file specified in the conf.
+
+    If there is an existing file in the destination, compare the new
+    contents with the existing contents. Return True if there is a difference.
+    """
     owner = conf.get('owner')
     # Check for user and group id in the environment.
     try:
@@ -268,6 +274,9 @@ def write_file(conf, data):
         tf.write(data)
         tf.flush()
         tf_name = tf.name
+
+    if os.path.exists(dest) and filecmp.cmp(tf_name, dest, shallow=False):
+        return False
     try:
         inst_cmd = ' '.join(['sudo', 'install', '-v',
                              '--no-target-directory',
@@ -277,6 +286,7 @@ def write_file(conf, data):
     except subprocess.CalledProcessError as exc:
         LOG.error(exc)
         LOG.exception(inst_cmd)
+    return True
 
 
 def generate_host_vars(zk):
