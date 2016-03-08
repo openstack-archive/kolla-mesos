e1cc1e10fe5df4c23eb0b689b4d9fd1e078dfa34
Angus Salkeld <asalkeld@mirantis.com>
2016-03-08 11:31:33 +1000

    CLI: implement config commands
    - list
    - show
    - set
    
    partial blueprint per-service-cli
    
    Change-Id: I15f714bdea16dfe963a9765f86d3933a647c1160

--- kolla_mesos/cli/config.py
+++ kolla_mesos/cli/config.py
@@ -13,33 +13,52 @@
 from cliff import command
 from cliff import lister
 from cliff import show
+from oslo_config import cfg
 from oslo_log import log
 
+from kolla_mesos.common import cli_utils
+from kolla_mesos.common import zk_utils
+
+CONF = cfg.CONF
+CONF.import_group('kolla', 'kolla_mesos.config.kolla')
 LOG = log.getLogger(__name__)
 
 
 class ConfigList(lister.Lister):
     """List Zookeeper variables."""
 
+    def get_parser(self, prog_name):
+        parser = super(ConfigList, self).get_parser(prog_name)
+        parser.add_argument('--path',
+                            default='/kolla/%s' % CONF.kolla.deployment_id)
+        return parser
+
     def take_action(self, parsed_args):
-        LOG.info('sending greeting')
-        LOG.debug('debugging')
-        self.app.stdout.write('hi!\n')
+        dd = zk_utils.list_all(parsed_args.path)
+        return (('Path', 'Value'), dd.items())
 
 
 class ConfigShow(show.ShowOne):
     """Show a Zookeeper variable value."""
 
+    def get_parser(self, prog_name):
+        parser = super(ConfigShow, self).get_parser(prog_name)
+        parser.add_argument('path')
+        return parser
+
     def take_action(self, parsed_args):
-        LOG.info('sending greeting')
-        LOG.debug('debugging')
-        self.app.stdout.write('hi!\n')
+        data = zk_utils.get_one(parsed_args.path)
+        return cli_utils.dict2columns(data, id_col='Path')
 
 
 class ConfigSet(command.Command):
     """Set a Zookeeper variable value."""
 
+    def get_parser(self, prog_name):
+        parser = super(ConfigSet, self).get_parser(prog_name)
+        parser.add_argument('path')
+        parser.add_argument('value')
+        return parser
+
     def take_action(self, parsed_args):
-        LOG.info('sending greeting')
-        LOG.debug('debugging')
-        self.app.stdout.write('hi!\n')
+        zk_utils.set_one(parsed_args.path, parsed_args.value)
