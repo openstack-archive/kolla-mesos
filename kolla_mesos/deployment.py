82ce449f1c0ee1f3b1daa71dbdf1d6e4db09cd9f
Angus Salkeld <asalkeld@mirantis.com>
2016-03-15 17:10:24 +1000

    CLI: implement deployment commands
    These are to replace the current
     kolla-mesos-deploy
     kolla-mesos-cleanup
    
    Make scripts that redirect to the new commands.
    
    partial blueprint per-service-cli
    Change-Id: Id730a1480b97e1059a7d04dd066b62156a028d20

--- kolla_mesos/deployment.py
+++ kolla_mesos/deployment.py
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os.path
+import os
 
 from oslo_config import cfg
 from oslo_log import log
@@ -19,8 +19,14 @@ from kolla_mesos.common import file_utils
 from kolla_mesos.common import jinja_utils
 from kolla_mesos.common import zk_utils
 from kolla_mesos import configuration
+from kolla_mesos import service
 
 CONF = cfg.CONF
+CONF.import_group('kolla', 'kolla_mesos.config.kolla')
+CONF.import_group('profiles', 'kolla_mesos.config.profiles')
+CONF.import_group('zookeeper', 'kolla_mesos.config.zookeeper')
+CONF.import_group('marathon', 'kolla_mesos.config.marathon')
+CONF.import_group('chronos', 'kolla_mesos.config.chronos')
 LOG = log.getLogger(__name__)
 
 
@@ -36,3 +42,28 @@ def write_openrc(out_filename):
         with open(out_filename, 'w') as f:
             f.write(content)
         LOG.info('Written OpenStack env to "%s"', out_filename)
+
+
+def run_deployment():
+    for proj in set(getattr(CONF.profiles, CONF.kolla.profile)):
+        for fn in os.listdir(os.path.join(CONF.service_dir, proj)):
+            if fn.endswith('.j2') and os.path.isfile(fn):
+                service.run_service(fn, CONF.service_dir)
+
+
+def get_deployment():
+    conf_opts = {}
+    conf_opts['deployment_id'] = CONF.kolla.deployment_id
+    conf_opts['profile'] = CONF.kolla.profile
+    conf_opts['service_dir'] = CONF.service_dir
+    conf_opts['image-name-template'] = '%s/%s-%s-{{ image }}:%s' % (
+        CONF.kolla.namespace, CONF.kolla.base, CONF.kolla.install_type,
+        CONF.kolla.tag)
+    conf_opts['marathon'] = CONF.marathon.host
+    conf_opts['mesos'] = CONF.marathon.host.replace('8080', '5050')
+    conf_opts['chronos'] = CONF.chronos.host
+    conf_opts['zookeeper'] = CONF.zookeeper.host
+    if False:
+        # TODO(asalkeld) if horizon is running and ready, get the url.
+        conf_opts['horizon'] = ''
+    return conf_opts
